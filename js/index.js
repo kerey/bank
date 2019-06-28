e82f3a7513327e2026818b65c48a1f853a262e42
Akzholtay <akzholtay.k@gmail.com>
2019-06-29 00:14:07 +0600

    add: first section, last part of first section: Erzhan

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+const links = [
+    { id: '#banner', active: true },
+    { id: '#beginning', active: true },
+];
+
 $( document ).ready(function() {
 
+});
+
+$(window).scroll(function(){
+    const scrollTop = $(this).scrollTop();
+    links.map(link => {
+        const top = $(link.id).offset().top;
+        const height = $(link.id).height();
+        const bottom = top + height;
+        const element = $('#sidebar a[href="'+link.id+'"] .btn-section');
+        if (scrollTop >= top && scrollTop < bottom) {
+            element.addClass('active');
+        } else {
+            element.removeClass('active');
+        }
+    });
 });
